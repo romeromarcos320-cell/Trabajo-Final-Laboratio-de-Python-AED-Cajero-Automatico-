1d7748acbdefc5df5a02c3e892a275ae7a9762e1
nicolasarzuaga7 <nicolasarzuaga7@gmail.com>
2026-07-08 16:52:47 -0300

    se agregaron más funciones
    - Se agregaron las funciones para depositar dinero y para tranferir dinero a otro usuario

--- operaciones.py
+++ operaciones.py
@@ -42,3 +42,53 @@ def extraer_dinero(usuario):
     print(f"\nNuevo saldo: ${cuentas_prueba[usuario]['saldo']:,.2f}")
     registrar_operacion(usuario, "Extracción de dinero", monto)
     
+def depositar_dinero(usuario):
+    try:
+        monto = float(input("\nIngrese el monto a depositar: $"))
+    except ValueError:
+        print("\n✗ Monto inválido. Debe ingresar un número.")
+        return
+    
+    if monto <= 0: # Si el monto es menor o igual a cero
+        print("\n✗ El monto debe ser mayor a cero.")
+        return
+    
+    cuentas_prueba[usuario]["saldo"] += monto
+    print(f"\n✓ Depósito exitoso.")
+    print(f"\nNuevo saldo: ${cuentas_prueba[usuario]['saldo']:,.2f}")
+    registrar_operacion(usuario, "Deposito", monto)
+    
+def transferir_dinero(usuario):
+    saldo = cuentas_prueba[usuario]["saldo"]
+    print(f"\nSaldo disponible: ${saldo:,.2f}")
+    
+    destinatario = input("\nIngrese el usuario del destinatario:").strip().lower()
+    
+    if destinatario == usuario:
+        print("\n✗ No puede transferirse dinero a sí mismo.")
+        return
+    
+    if destinatario not in cuentas_prueba:
+        print(f"\n✗ Usuario '{destinatario}' no encontrado.")
+        return
+    
+    try:
+        monto = float(input("\nIngrese el monto a transferir: $"))
+    except ValueError:
+        print("\n✗ Monto inválido. Debe ingresar un número.")
+        return
+    
+    if monto <= 0: # Si el monto es menor o igual a cero
+        print("\n✗ El monto debe ser mayor a cero.")
+        return
+    
+    if monto > saldo: # Si el monto supera el saldo disponible
+        print(f"\n✗ Saldo insuficiente. Su saldo es ${saldo:,.2f}")
+        return 
+    
+    cuentas_prueba[usuario]["saldo"] -= monto
+    cuentas_prueba[destinatario]["saldo"] += monto
+    
+    print(f"\n✓ Transferencia exitosa de ${monto:,.2f} a {destinatario.capitalize()}.")
+    print(f"\nNuevo saldo: ${cuentas_prueba[usuario]['saldo']:,.2f}")
+    registrar_operacion(usuario, f"Transferencia a {destinatario}", monto)  
